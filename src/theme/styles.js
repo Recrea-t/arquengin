47e7c54ee55d8c3e2a3685ffcbdcbee49238ab6d
David Gay i Tello <david.gay.tello@protonmail.com>
2021-04-15 10:09:38 +0200

    Initial commit from gatsby: (git@github.com:Recrea-t/gatsby-recreat-starter.git)

---
+++ src/theme/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+  global: {
+    html: {
+      //scrollBehavior: "smooth",
+    },
+    body: {
+      color: "dimGray.500",
+    },
+    ".markdown": {
+      li: {
+        margin: "0 !important",
+      },
+    },
+  },
+}
+
+export default styles
